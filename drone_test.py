689dc6dde67bef21fba0cc0d239f3b04868a60d8
Julia RONNEBERGER <jronnebe@intern1.cs.ucl.ac.uk>
2019-07-17 10:46:34 +0100

    first commit

---
+++ drone_test.py
@@ -0,0 +1,14 @@
+import gym
+import gym_drone
+env = gym.make('drone-v0')
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(150):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
